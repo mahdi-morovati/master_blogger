5b6bbe6e7c6ef05132f5c0647a339bc1ea213e6b
Mahdi Morovati <morovati155@gmail.com>
2024-08-05 15:34:18 +0330

    create other projects

---
+++ MB.Infrstracture.Core/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using MB.Application;
+using MB.Application.Contracts.ArticleCategory;
+using MB.Domain.ArticleCategoryAgg;
+using MB.Infrastructure.EFCore;
+using MB.Infrastructure.EFCore.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MB.Infrstracture.Core;
+
+public class Bootstrapper
+{
+    public static void Config(WebApplicationBuilder builder, string connectionString)
+    {
+        builder.Services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
+        builder.Services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
+
+        builder.Services.AddDbContext<MasterBloggerContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+}
